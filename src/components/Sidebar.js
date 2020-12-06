4402022bc0f52670b35a4c5602c4bff6e08b38ff
Sheik Sena Reddy <sheiksenareddy@gmail.com>
2020-12-06 13:39:04 +0530

    Minor

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,7 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { ProSidebar, Menu, MenuItem, SubMenu, SidebarContent } from 'react-pro-sidebar';
+import { ProSidebar, Menu, MenuItem, SidebarContent } from 'react-pro-sidebar';
 import 'react-pro-sidebar/dist/css/styles.css';
-import { CheckSquareFill, GridFill, HddRackFill } from 'react-bootstrap-icons';
+import { CheckSquareFill, GridFill } from 'react-bootstrap-icons';
 
 function Sidebar() {
 
@@ -11,29 +11,6 @@ function Sidebar() {
                 <SidebarContent>
                     <Menu icon={<CheckSquareFill />}>
                         <MenuItem icon={<GridFill />}> <a href="/register">Dashboard</a></MenuItem>
-                        <SubMenu title="Components" icon={<HddRackFill />}>
-                            <MenuItem>Component 1</MenuItem>
-                            <MenuItem>Component 2</MenuItem>
-                        </SubMenu>
-                        <MenuItem icon={<GridFill />}>Component 1</MenuItem>
-                        <MenuItem icon={<GridFill />}>Component 2</MenuItem>
-                        <MenuItem icon={<GridFill />}>Component 1</MenuItem>
-                        <MenuItem icon={<GridFill />}>Component 2</MenuItem>
-                        <MenuItem icon={<GridFill />}>Component 1</MenuItem>
-                        <MenuItem icon={<GridFill />}>Component 2</MenuItem>
-                        <MenuItem icon={<GridFill />}>Component 1</MenuItem>
-                        <MenuItem></MenuItem>
-                        <MenuItem></MenuItem>
-                        <MenuItem></MenuItem>
-                        <MenuItem></MenuItem>
-                        <MenuItem></MenuItem>
-                        <MenuItem></MenuItem>
-                        <MenuItem></MenuItem>
-                        <MenuItem></MenuItem>
-                        <MenuItem></MenuItem>
-                        <MenuItem></MenuItem>
-                        <MenuItem></MenuItem>
-                        <MenuItem></MenuItem>
                     </Menu>
                 </SidebarContent>
             </ProSidebar>
