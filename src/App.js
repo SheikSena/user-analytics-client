44794cd04b72f1e62e958400b41b1809454a5c0b
Sheik Sena Reddy <sheiksenareddy@gmail.com>
2020-12-06 12:01:57 +0530

    Minor

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Sidebar from './components/Sidebar'
 import Home from './components/Home'
-import { Col, Row } from 'react-bootstrap';
 function App() {
 
   return (
