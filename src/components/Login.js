cdc85d1c4440e1eae939f221eb6374870fa3c09a
Sheik Sena Reddy <sheiksenareddy@gmail.com>
2020-12-30 12:13:01 +0530

    UI

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,6 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { Component } from 'react';
-import { Card, CardContent, Grid, Button } from '@material-ui/core'
+import { Grid, Button, Container, Typography, Link, Box } from '@material-ui/core'
 import { Alert } from 'react-bootstrap'
 import { connect } from 'react-redux';
 import { authenticateUser } from '../services/User/Auth/AuthActions'
@@ -16,6 +16,19 @@ const MyButton = styled(Button)({
     height: 40,
     padding: '0 30px',
 });
+
+function Copyright() {
+    return (
+        <Typography variant="body2" color="textSecondary" align="center">
+            {'Copyright © '}
+            <Link color="inherit" href="https://user-analytics.vercel.app/">
+                User Aanalytics
+        </Link>{' '}
+            {new Date().getFullYear()}
+            {'.'}
+        </Typography>
+    );
+}
 class Login extends Component {
 
     constructor(props) {
@@ -60,128 +73,121 @@ class Login extends Component {
 
     render() {
         const { email, password, error, displayLoginDiv, displayRegDiv } = this.state;
-
         return (
             <div className="col d-flex justify-content-center" style={{ backgroundColor: 'white', height: '100%', paddingTop: '70px' }}>
                 {displayLoginDiv ? <div style={{ height: '50%' }}>
                     {error && <Alert variant="danger"> {error}</Alert>}
-                    <Grid>
-                        <Grid justify="space-between"
-                            alignItems="center">
-                            <Card >
-                                {/* <CardHeader title="LOGIN" style={{ textAlign: 'center', 'fontWeight': 'bold' }} /> */}
-                                <CardContent>
-                                    <form noValidate>
-                                        <TextField variant="outlined" margin="normal"
-                                            required
-                                            fullWidth
-                                            id="email"
-                                            label="Email Address"
-                                            name="email"
-                                            autoComplete="off"
-                                            value={email}
-                                            onChange={this.handleChange}
-                                        />
-                                        <TextField
-                                            variant="outlined"
-                                            margin="normal"
-                                            required
-                                            fullWidth
-                                            name="password"
-                                            label="Password"
-                                            type="password"
-                                            id="password"
-                                            autoComplete="current-password"
-                                            value={password}
-                                            onChange={this.handleChange}
-                                        />
-                                        <p>Don't have an account? click <a href="#" onClick={this.showRegDiv.bind(this)}>here</a> to create an account.</p>
-                                    </form>
-                                    <MyButton
+                    <Container component="main" maxWidth="xs">
+                        <Typography component="h1" variant="h5" align='center'>LOGIN</Typography>
+                        <br></br>
+                        <form noValidate>
+                            <Grid container spacing={0}>
+                                <Grid item xs={12}>
+                                    <TextField variant="outlined" margin="normal"
+                                        required
                                         fullWidth
-                                        type="submit"
-                                        variant="contained"
-                                        color="primary"
-                                        onClick={this.validateUser}
-                                    >
-                                        LOGIN
-                            </MyButton >
-                                </CardContent>
-                            </Card>
-                        </Grid>
-                    </Grid>
+                                        id="email"
+                                        label="Email Address"
+                                        name="email"
+                                        autoComplete="off"
+                                        value={email}
+                                        onChange={this.handleChange}
+                                        autoFocus
+                                    />
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <TextField
+                                        variant="outlined"
+                                        margin="normal"
+                                        required
+                                        fullWidth
+                                        name="password"
+                                        label="Password"
+                                        type="password"
+                                        id="password"
+                                        autoComplete="current-password"
+                                        value={password}
+                                        onChange={this.handleChange}
+                                    />
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <Typography variant="body2">Don't have an account? click <a href="#" onClick={this.showRegDiv.bind(this)}>here</a> to create an account.</Typography>
+                                </Grid>
+                            </Grid>
+                        </form>
+                        <br></br>
+                        <MyButton fullWidth onClick={this.validateUser}>  LOGIN</MyButton >
+                        <Box mt={5}>
+                            <br></br>
+                            <Copyright />
+                        </Box>
+                    </Container>
                 </div> : null}
                 {displayRegDiv ?
                     <div style={{ height: '50%' }}>
-                        <Grid>
-                            <Grid justify="space-between"
-                                alignItems="center" >
-                                <Card >
-                                    {/* <CardHeader title="SIGN UP" style={{ textAlign: 'center', 'fontWeight': 'bold' }} /> */}
-                                    <CardContent>
-                                        <form noValidate>
-                                            <TextField variant="outlined" margin="normal"
-                                                required
-                                                fullWidth
-                                                id="firstName"
-                                                label="First Name"
-                                                name="firstName"
-                                                autoComplete="off"
-                                                type="text"
-                                            />
-                                            <TextField variant="outlined" margin="normal"
-                                                fullWidth
-                                                id="lastName"
-                                                label="Last Name"
-                                                name="lastName"
-                                                autoComplete="off"
-                                                type="text"
-                                            />
-                                            <TextField variant="outlined" margin="normal"
-                                                required
-                                                fullWidth
-                                                id="email"
-                                                label="Email Address"
-                                                name="email2"
-                                                autoComplete="off"
-                                                type="email"
-                                            />
-                                            <TextField
-                                                variant="outlined"
-                                                margin="normal"
-                                                required
-                                                fullWidth
-                                                name="password1"
-                                                label="Password"
-                                                type="password"
-                                                id="password1"
-                                                autoComplete="current-password1"
-                                            />
-                                            <TextField
-                                                variant="outlined"
-                                                margin="normal"
-                                                required
-                                                fullWidth
-                                                name="password2"
-                                                label="Confirm Password"
-                                                type="password"
-                                                id="password2"
-                                                autoComplete="current-password2"
-                                            />
-                                            <p>Already have an account? click <a href="#" onClick={this.showLoginDiv.bind(this)}>here</a> to login.</p>
-                                        </form>
-                                        <MyButton
+                        <Container component="main" maxWidth="xs">
+                            <Typography component="h1" variant="h5" align='center'>SIGN UP</Typography>
+                            <br></br>
+                            <form noValidate>
+                                <Grid container spacing={2}>
+                                    <Grid item xs={12}>
+                                        <TextField
+                                            autoComplete="fname"
+                                            name="firstName"
+                                            variant="outlined"
+                                            required
                                             fullWidth
-                                            type="submit"
-                                            variant="contained"
-                                            color="primary"
-                                        >
-                                            SIGN UP
-                            </MyButton>
-                                    </CardContent>
-                                </Card>
-                            </Grid>
-                        </Grid>
+                                            id="firstName"
+                                            label="First Name"
+                                            autoFocus
+                                        />
+                                    </Grid>
+                                    <Grid item xs={12}>
+                                        <TextField
+                                            variant="outlined"
+                                            fullWidth
+                                            id="lastName"
+                                            label="Last Name"
+                                            name="lastName"
+                                            autoComplete="lname"
+                                        />
+                                    </Grid>
+                                    <Grid item xs={12}>
+                                        <TextField
+                                            variant="outlined"
+                                            required
+                                            fullWidth
+                                            id="email1"
+                                            label="Email Address"
+                                            name="email1"
+                                            autoComplete="email"
+                                        />
+                                    </Grid>
+                                    <Grid item xs={12}>
+                                        <TextField
+                                            variant="outlined"
+                                            required
+                                            fullWidth
+                                            name="password1"
+                                            label="Password"
+                                            type="password"
+                                            id="password1"
+                                            autoComplete="current-password"
+                                        />
+                                    </Grid>
+                                    <Grid item xs={12}>
+                                        <Typography variant="body2">Already have an account? click <a href="#" onClick={this.showLoginDiv.bind(this)}>here</a> to login.</Typography>
+                                    </Grid>
+                                </Grid>
+                            </form>
+                            <br></br>
+                            <MyButton fullWidth>SIGN UP</MyButton>
+                            <Box mt={5}>
+                                <br></br>
+
+                                <Copyright />
+                            </Box>
+                        </Container>
                     </div> : null}
             </div>
         );
